cb91fecba4a5f0f1ac426b4982462f5ce1b285c3
rishabh477 <rishabhagrawal.477@gmail.com>
2018-11-28 16:19:17 +0530

    new change

--- about.js
+++ about.js
@@ -64,7 +64,7 @@ $(document).ready(function(){
             $(this).prop('Counter',0).animate({
               Counter: $(this).text()},
                 {
-                  duration: 2000,
+                  duration: 3000,
                   easing: 'swing',
                   step: function (now) {
                     $(this).text(Math.ceil(now));
